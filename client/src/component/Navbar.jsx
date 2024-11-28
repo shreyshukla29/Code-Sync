c55a27cc890331e2436e0c5837cfa049937198e7
shrey <shreyshukla101217@gmail.com>
2024-11-28 18:21:12 +0530

    update navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,11 +1,14 @@
 
 import { Link } from 'react-scroll';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate= useNavigate();
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-80 text-white shadow-lg z-10">
       <div className="max-w-screen-lg  flex justify-between items-center py-4 px-6">
-        <h1 className="text-xl font-bold ">CodeSync</h1>
+        <h1 className="text-xl font-bold cursor-pointer"
+        onClick={()=>navigate('/')}>CodeSync</h1>
         <ul className="flex space-x-6">
           <li>
             <Link
@@ -16,6 +19,12 @@ const Navbar = () => {
             >
               Home
             </Link>
+          </li>
+          <li
+            
+              className="cursor-pointer text-gray-300  hover:text-white transition duration-300"
+           onClick = {()=>navigate('/dashboard')} >
+              Dashboard
           </li>
           <li>
             <Link
